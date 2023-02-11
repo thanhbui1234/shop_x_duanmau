b5fb71f25bc1463777d936ddaf9cd017cf8bf4ec
thanh <miktenthanh@gmail.com>
2023-02-11 07:29:22 +0700

    commmitss ok

--- admin/model/function.php
+++ admin/model/function.php
@@ -437,3 +437,139 @@ function selectCmtProd($id)
         $dataSelectName = $statement->fetchAll();
     }
 }
+
+function showRequest()
+{
+
+    global $conn;
+    $sql = "SELECT * FROM requests_admin ";
+    $statement = $conn->prepare($sql);
+    $statement->execute();
+    global $dataRequests;
+    $dataRequests = $statement->fetchAll();
+
+}
+function selectUserName($id)
+{
+    if (isset($id)) {
+        global $conn;
+        $sql = " select * from user where id = $id ";
+        $statement = $conn->prepare($sql);
+        $statement->execute();
+        global $dataUsername;
+        $dataUsername = $statement->fetchAll();
+    }
+}
+function actionAdmin()
+{
+    if (isset($_GET['accpetAdmin'])) {
+
+        global $conn;
+        $id = $_GET['accpetAdmin'];
+        $sql = " update user set role = 1 where  id = $id";
+        $statement = $conn->prepare($sql);
+        if ($statement->execute()) {
+            echo "<script>
+           Swal.fire(
+  'Bạn đã cập nhập thành công!',
+  ':333!',
+  'success'
+)</script>";
+        }
+
+    }
+}
+function deleteRequest()
+{
+
+    if (isset($_GET['delete'])) {
+
+        global $conn;
+        $sql = "DELETE FROM requests_admin where id = $_GET[delete]";
+        $statement = $conn->prepare($sql);
+        if ($statement->execute()) {
+            header('location: /shop_xx/admin//index.php?act=requestUser');
+        }
+
+    }
+}
+function showAlluser()
+{
+    global $conn;
+    $sql = " select * from user";
+    $statement = $conn->prepare($sql);
+    $statement->execute();
+    global $dataAllUsers;
+    $dataAllUsers = $statement->fetchAll();
+}
+
+function deleteUser()
+{
+
+    if (isset($_GET['delete'])) {
+        global $conn;
+        $sql = " delete from user where id = $_GET[delete]";
+        $statement = $conn->prepare($sql);
+        if ($statement->execute()) {
+            header('location: /shop_xx/admin//index.php?act=listUser');
+        }
+
+    }
+}
+function roleUser()
+{
+
+    if (isset($_GET['admin'])) {
+        global $conn;
+        $sql = " update user set role = 1 where id = $_GET[admin] ";
+        $statement = $conn->prepare($sql);
+        if ($statement->execute()) {
+            header('location: /shop_xx/admin//index.php?act=listUser');
+        }
+
+    }
+    if (isset($_GET['normal'])) {
+        global $conn;
+        $sql = " update user set role = 0 where id = $_GET[normal] ";
+        $statement = $conn->prepare($sql);
+        if ($statement->execute()) {
+            header('location: /shop_xx/admin//index.php?act=listUser');
+        }
+
+    }
+
+}
+
+function coutAll()
+{
+
+    global $conn;
+
+    $sqlUser = " select count(*) from user  ";
+    $statement = $conn->prepare($sqlUser);
+    $statement->execute();
+    global $users;
+    $users = $statement->fetchColumn();
+
+    $sqlCategories = " select count(*) from categories  ";
+    $statement = $conn->prepare($sqlCategories);
+    $statement->execute();
+    global $categories;
+
+    $categories = $statement->fetchColumn();
+
+    $sqlProduct = " select count(*) from products  ";
+    $statement = $conn->prepare($sqlProduct);
+    $statement->execute();
+    global $products;
+
+    $products = $statement->fetchColumn();
+
+    $sqlCommnets = " select count(*) from commnets  ";
+    $statement = $conn->prepare($sqlCommnets);
+    $statement->execute();
+    global $commnets;
+
+    $commnets = $statement->fetchColumn();
+
+}
